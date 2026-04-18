f682def6c682c68a5417eab0caf3b3f77e8cf350
ArtEdGuru <lovsart@gmail.com>
2026-04-18 12:51:27 -0400

    remove fix scripts

--- fix.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('public/index.html', 'utf8');
-c = c.replace(
-  'const formattedHTML = formatLesson(lastLessonText);',
-  'const formattedHTML = formatLesson(lastLessonText);\n    const hasProgressDoc = addons.includes(\'progressdoc\');\n    const hasRubric = addons.includes(\'rubric\');\n    const formsHTML = (hasProgressDoc ? getProgressDocHTML() : \'\') + (hasRubric ? getRubricHTML() : \'\');'
-);
-fs.writeFileSync('public/index.html', c);
-console.log('Done!');
--- fix2.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('public/index.html', 'utf8');
-c = c.replace(
-  '<div class="lesson-content">${formattedHTML}</div>',
-  '<div class="lesson-content">${formattedHTML}</div>${formsHTML}'
-);
-fs.writeFileSync('public/index.html', c);
-console.log('Done!');
